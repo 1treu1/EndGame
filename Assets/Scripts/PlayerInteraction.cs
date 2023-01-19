021802b020ec3c082e9f467dbb84ac041980fb12
treu <luis.medina@utp.edu.co>
2023-01-19 17:41:53 -0500

    Creo que ya 2

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -6,9 +6,10 @@ public class PlayerInteraction : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject deck;
+    private GameObject roof;
     void Start()
     {
-        
+        roof = GameObject.FindGameObjectWithTag("Roof");
     }
 
     // Update is called once per frame
@@ -18,24 +19,35 @@ public class PlayerInteraction : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
-       
+        Debug.Log(other.name);
         if (other.CompareTag("Deck"))
         {
             Debug.Log("Deck");
             deck = other.gameObject.transform.GetChild(0).gameObject;
             deck.SetActive(false);
         }
+        if (other.CompareTag("Floor"))
+        {
+            Debug.Log("Floor");
+            roof.SetActive(false);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-
+        Debug.Log(other.name);
         if (other.CompareTag("Deck"))
         {
             Debug.Log("Exit Deck");
             deck = other.gameObject.transform.GetChild(0).gameObject;
             deck.SetActive(true);
         }
-
+        if (other.CompareTag("Floor"))
+        {
+            Debug.Log("Floor");
+            roof.SetActive(true);
+        }
     }
+
 }
+
